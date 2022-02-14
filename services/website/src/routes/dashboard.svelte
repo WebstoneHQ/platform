f52e17e3a114f537bf8c3c0051367d82e4e8afe6
Mike Nikles <mikenikles@pm.me>
2022-02-14 00:08:36 +0000

    chore(dashboard): add a placeholder dashboard with a link to the community

---
+++ services/website/src/routes/dashboard.svelte
@@ -0,0 +1,33 @@
+<script lang="ts" context="module">
+  import type { Load } from "@sveltejs/kit";
+
+  export const load: Load = ({ session }) => {
+    if (session.user) {
+      return {
+        props: {},
+      };
+    }
+    return {
+      status: 302,
+      redirect: "/",
+    };
+  };
+</script>
+
+<h1 class="mb-4 text-4xl">Dashboard</h1>
+
+<p>You arrived here a bit early - welcome 👋.</p>
+<p>
+  We are actively working on this project, please join our Discord server to get
+  exclusive project updates and access to pre-release pricing.
+</p>
+
+<div class="mx-auto mt-4 w-1/2 md:w-1/3">
+  <a
+    href="https://discord.gg/EzJDBT6uRv"
+    target="_blank"
+    rel="noreferrer"
+    class="block w-full rounded-md border border-transparent bg-orange-500 px-5 py-3 text-center text-base font-medium text-slate-900 shadow hover:bg-orange-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10"
+    >Join Community</a
+  >
+</div>
