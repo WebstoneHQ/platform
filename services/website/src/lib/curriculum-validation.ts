8aa0139e52dcd33cc1fc8aa93d1f3be9c4e2faaf
Mike Nikles <788827+mikenikles@users.noreply.github.com>
2022-02-16 20:50:34 -0800

    Update services/website/src/lib/curriculum-validation.ts

--- services/website/src/lib/curriculum-validation.ts
+++ services/website/src/lib/curriculum-validation.ts
@@ -144,7 +144,7 @@ export default {
         },
         rest: {
           api: {
-            nextjs: {
+            nuxtjs: {
               database: {
                 mongodb: {},
                 mysql: {},
