14ec758efd1b77981c5f4b30033e33e55b36d6c6
Mike Nikles <mikenikles@pm.me>
2022-05-15 21:25:29 +0000

    feat(website): prevent anonymous access to lesson content

--- services/website/src/routes/courses/[courseid]/lessons/[lessonid].svelte
+++ services/website/src/routes/courses/[courseid]/lessons/[lessonid].svelte
@@ -1,7 +1,7 @@
 <script lang="ts" context="module">
   import type { Load } from "@sveltejs/kit";
 
-  export const load: Load = async ({ fetch, params, stuff }) => {
+  export const load: Load = async ({ fetch, params, session, stuff }) => {
     // TODO: Check if the authenticated student has purchased this course. If not, redirect to ../ (course page) and display a message to purchase the course
     if (stuff.course) {
       const response = await fetch(
@@ -17,6 +17,7 @@
       return {
         props: {
           lessonContent: await response.text(),
+          user: session.user,
         },
       };
     }
@@ -30,9 +31,29 @@
 
 <script lang="ts">
   import { marked } from "marked";
+  import PreorderButtonGumroad from "$lib/components/preorder-button-gumroad.svelte";
+  import PreorderBenefits from "$lib/components/preorder-benefits.svelte";
+
   export let lessonContent: string;
+  export let user: User;
 </script>
 
-<article class="prose dark:prose-invert lg:prose-xl">
-  {@html marked.parse(lessonContent)}
-</article>
+{#if user}
+  <article class="prose dark:prose-invert lg:prose-xl">
+    {@html marked.parse(lessonContent)}
+  </article>
+{:else}
+  <div class="py-6">
+    <div class="px-4 sm:px-6 md:px-0">
+      <h1 class="text-2xl font-semibold">Sign in required</h1>
+    </div>
+    <div class="px-4 sm:px-6 md:px-0">
+      <p>To access this lesson, please sign up below.</p>
+    </div>
+
+    <div class="px-4 sm:px-6 md:px-0">
+      <PreorderButtonGumroad />
+      <PreorderBenefits />
+    </div>
+  </div>
+{/if}
