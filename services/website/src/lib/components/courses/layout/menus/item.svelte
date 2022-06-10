f8302d6270607b1926b85e8899b64acb92067b07
Mike Nikles <mikenikles@pm.me>
2022-06-10 01:47:49 +0000

    chore(website): change the lesson icon in the menu

--- services/website/src/lib/components/courses/layout/menus/item.svelte
+++ services/website/src/lib/components/courses/layout/menus/item.svelte
@@ -13,21 +13,23 @@
   class:md:ml-8="{href.match(/lessons\/\d{2}-\d[1-9]-/)}"
   class="group flex items-center rounded-md py-2 px-2 text-base font-medium md:text-sm"
 >
-  <!-- Heroicon name: outline/users -->
+  <!-- Heroicon name: outline/academic-cap -->
   <svg
-    class:group-hover:text-gray-500="{!isActive}"
-    class="mr-4 h-6 w-6 flex-shrink-0 md:mr-3"
     xmlns="http://www.w3.org/2000/svg"
+    class="mr-1 h-6 w-6"
     fill="none"
     viewBox="0 0 24 24"
     stroke="currentColor"
-    aria-hidden="true"
+    stroke-width="2"
   >
+    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+    <path
+      d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
+    ></path>
     <path
       stroke-linecap="round"
       stroke-linejoin="round"
-      stroke-width="2"
-      d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z"
+      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
     ></path>
   </svg>
   <slot />
