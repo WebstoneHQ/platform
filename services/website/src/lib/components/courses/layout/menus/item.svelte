e92ed69813b230cc04cf78460d86238ec5fd3dde
Mike Nikles <mikenikles@pm.me>
2022-07-04 04:40:19 +0000

    refactor(website): use numbered lesson nav items, no icon

--- services/website/src/lib/components/courses/layout/menus/item.svelte
+++ services/website/src/lib/components/courses/layout/menus/item.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   export let isActive = false;
   export let href: string;
+
+  const lessonId = href.split("/").pop();
+  const lessonNumber =
+    lessonId.split("-")[1] === "00"
+      ? lessonId.split("-")[0]
+      : `${lessonId.split("-")[0]}.${lessonId.split("-")[1]}`;
 </script>
 
 <a
@@ -13,24 +19,5 @@
   class:md:ml-8="{href.match(/lessons\/\d{2}-\d[1-9]-/)}"
   class="group flex items-center rounded-md py-2 px-2 text-base font-medium md:text-sm"
 >
-  <!-- Heroicon name: outline/academic-cap -->
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    class="mr-1 h-6 w-6"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke="currentColor"
-    stroke-width="2"
-  >
-    <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
-    <path
-      d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"
-    ></path>
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
-    ></path>
-  </svg>
-  <slot />
+  {lessonNumber}. <slot />
 </a>
